1123be86db1d7b3687150b39299c9893d00a435b
Billy Levin <billy.levin97@gmail.com>
2026-06-20 08:08:26 +0100

    file_picker: add search

--- src/file_picker.rs
+++ src/file_picker.rs
@@ -1,4 +1,10 @@
-use crossterm::event::Event;
+use std::cmp;
+
+use crossterm::event::{
+    Event,
+    KeyCode,
+    KeyModifiers,
+};
 use fff_search::{
     FFFMode,
     FilePicker as FFFFilePicker,
@@ -8,6 +14,7 @@ use fff_search::{
     SharedFilePicker,
     SharedFrecency,
 };
+use unicode_segmentation::UnicodeSegmentation as _;
 
 use crate::{
     buffer::Buffer,
@@ -15,8 +22,14 @@ use crate::{
         EventContext,
         EventOutcome,
     },
+    keymap::KeyBinding,
     style::Style,
+    text::{
+        ByteIndex,
+        text_width,
+    },
     ui::{
+        Columns,
         Dimensions,
         Layer,
         LayerKind,
@@ -59,25 +72,48 @@ impl FileIndex {
 
 pub(crate) struct FilePicker {
     picker: SharedFilePicker,
+    search_term: String,
+    cursor_position: Option<Position>,
+    files: Vec<String>,
 }
 
 impl FilePicker {
-    pub(crate) const fn new(picker: SharedFilePicker) -> Self {
-        Self { picker }
+    pub(crate) fn new(picker: SharedFilePicker) -> Self {
+        let mut this = Self {
+            picker,
+            search_term: String::new(),
+            cursor_position: None,
+            files: Vec::new(),
+        };
+
+        this.update_files();
+
+        this
     }
 
-    fn files(&self) -> Vec<Line<'_>> {
+    fn apply_action(&mut self, action: Action) {
+        match action {
+            Action::InsertChar(ch) => {
+                self.search_term.push(ch);
+                self.update_files();
+            }
+            Action::RemoveChar => {
+                self.search_term.pop();
+                self.update_files();
+            }
+        }
+    }
+
+    fn update_files(&mut self) {
         let Ok(guard) = self.picker.read() else {
-            return Vec::new();
+            return;
         };
 
-        let Some(picker) = guard.as_ref() else {
-            return Vec::new();
-        };
+        let Some(picker) = guard.as_ref() else { return };
 
-        picker
+        self.files = picker
             .fuzzy_search(
-                &QueryParser::default().parse(""),
+                &QueryParser::default().parse(&self.search_term),
                 None,
                 FuzzySearchOptions {
                     max_threads: 0,
@@ -86,8 +122,22 @@ impl FilePicker {
             )
             .items
             .iter()
-            .map(|item| Line::new(vec![Span::new(item.relative_path(picker))]))
-            .collect()
+            .map(|item| item.relative_path(picker))
+            .collect();
+    }
+
+    fn horizontal_scroll(&self, text_rectangle: &Rectangle) -> ByteIndex {
+        let mut scroll = ByteIndex::new(0);
+        let mut width = Columns::new(0);
+
+        for grapheme in self.search_term.graphemes(true).rev() {
+            width += text_width(grapheme);
+            if width >= text_rectangle.width() {
+                scroll += ByteIndex::new(grapheme.len());
+            }
+        }
+
+        scroll
     }
 }
 
@@ -104,21 +154,63 @@ impl Layer for FilePicker {
 
         let (input_rectangle, file_list_rectangle) = rectangle.split_at_row(Rows::new(3));
 
-        let _input_text_rectangle =
-            buffer.draw_border(&input_rectangle, Style::COMMAND_LIST_BORDER);
+        let input_rectangle = buffer.draw_border(&input_rectangle, Style::COMMAND_LIST_BORDER);
+
+        let text = self
+            .search_term
+            .get(self.horizontal_scroll(&input_rectangle).value()..)
+            .expect("horizontal scroll should give a byte index on a char boundary");
+
+        buffer.render_span(
+            &Span::new(text).with_style(Style::COMMAND_LIST_INPUT_TEXT),
+            &input_rectangle.offset(),
+            &input_rectangle,
+        );
 
         let file_list_rectangle =
             buffer.draw_border(&file_list_rectangle, Style::COMMAND_LIST_BORDER);
 
-        buffer.render_lines(self.files(), &file_list_rectangle);
+        buffer.render_lines(
+            self.files
+                .iter()
+                .map(|file| Line::new(vec![Span::new(file)]))
+                .collect(),
+            &file_list_rectangle,
+        );
+
+        self.cursor_position = Some(input_rectangle.offset().col_offset(cmp::min(
+            text_width(text),
+            input_rectangle.width() - Columns::new(1),
+        )));
     }
 
-    fn handle_event(&mut self, _event: &Event, _event_context: &mut EventContext) -> EventOutcome {
-        EventOutcome::Unhandled
+    fn handle_event(&mut self, event: &Event, _event_context: &mut EventContext) -> EventOutcome {
+        match *event {
+            Event::Key(key_event) => {
+                let key_binding = KeyBinding::from(key_event);
+
+                let action = match (key_binding.code(), key_binding.modifiers()) {
+                    (KeyCode::Char(ch), KeyModifiers::NONE) => Some(Action::InsertChar(ch)),
+                    (KeyCode::Backspace, KeyModifiers::NONE) => Some(Action::RemoveChar),
+                    _ => None,
+                };
+
+                action.map_or(EventOutcome::Unhandled, |a| {
+                    self.apply_action(a);
+                    EventOutcome::Handled
+                })
+            }
+
+            Event::FocusGained
+            | Event::FocusLost
+            | Event::Mouse(_)
+            | Event::Paste(_)
+            | Event::Resize(_, _) => EventOutcome::Unhandled,
+        }
     }
 
     fn visual_cursor_position(&self) -> Option<Position> {
-        None
+        self.cursor_position
     }
 
     fn handle_internal_events(&mut self) -> EventOutcome {
@@ -128,4 +220,10 @@ impl Layer for FilePicker {
     fn kind(&self) -> Option<LayerKind> {
         Some(LayerKind::FilePicker)
     }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Action {
+    InsertChar(char),
+    RemoveChar,
 }
