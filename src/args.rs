95989a6ea0a010e050e0535367b94b99ad632506
Billy Levin <billy.levin97@gmail.com>
2026-07-02 22:32:04 +0100

    global: set up logging

--- src/args.rs
+++ src/args.rs
@@ -4,4 +4,34 @@ use std::path::PathBuf;
 pub struct Args {
     #[arg(value_hint = clap::ValueHint::FilePath)]
     pub(crate) file_path: PathBuf,
+
+    #[arg(short, long, value_enum, default_value_t = LogLevel::Warn, help = "Log level")]
+    pub(crate) log_level: LogLevel,
+}
+
+impl Args {
+    #[must_use]
+    pub const fn log_level(&self) -> LogLevel {
+        self.log_level
+    }
+}
+
+#[derive(Clone, Copy, Debug, clap::ValueEnum)]
+#[value(rename_all = "UPPER")]
+pub enum LogLevel {
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl From<LogLevel> for log::LevelFilter {
+    fn from(level: LogLevel) -> Self {
+        match level {
+            LogLevel::Warn => Self::Warn,
+            LogLevel::Info => Self::Info,
+            LogLevel::Debug => Self::Debug,
+            LogLevel::Trace => Self::Trace,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::io;
+use std::{
+    fs,
+    time::SystemTime,
+};
 
+use anyhow::Context as _;
 use clap::Parser as _;
 use iron::{
-    args::Args,
+    args::{
+        Args,
+        LogLevel,
+    },
     terminal::Terminal,
 };
 
-fn main() -> io::Result<()> {
+fn main() -> anyhow::Result<()> {
     #[cfg(feature = "profile")]
     let server_addr = format!("0.0.0.0:{}", puffin_http::DEFAULT_PORT);
     #[cfg(feature = "profile")]
@@ -14,5 +21,40 @@ fn main() -> io::Result<()> {
     #[cfg(feature = "profile")]
     puffin::set_scopes_on(true);
 
-    Terminal::run(Args::parse())
+    let args = Args::parse();
+    init_logging(args.log_level())?;
+    Terminal::run(args)?;
+
+    Ok(())
+}
+
+fn init_logging(level: LogLevel) -> anyhow::Result<()> {
+    let dirs = directories::ProjectDirs::from("", "", "iron")
+        .context("failed to determine project dirs")?;
+
+    let log_file = dirs
+        .state_dir()
+        .unwrap_or_else(|| dirs.data_local_dir())
+        .join("iron.log");
+
+    fs::create_dir_all(
+        log_file
+            .parent()
+            .context("log file must have a parent directory")?,
+    )?;
+
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} {} [{}]: {}",
+                humantime::format_rfc3339_millis(SystemTime::now()),
+                record.target(),
+                record.level(),
+                message
+            ));
+        })
+        .level(log::LevelFilter::from(level))
+        .chain(fern::log_file(&log_file)?)
+        .apply()
+        .context("failed to init logging")
 }
